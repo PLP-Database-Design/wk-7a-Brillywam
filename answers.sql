f87765237aae91a493e8f57b9c2abeed0b0f44f9
Brillywam <mweshwam5@gmail.com>
2025-05-03 23:03:17 +0300

    Week 7 Assignment

---
+++ answers.sql
@@ -0,0 +1,50 @@
+-- Qn 1 Achieving 1NF (First Normal Form)
+
+-- Create Products table
+CREATE TABLE Products (
+    ProductID INT PRIMARY KEY AUTO_INCREMENT, -- Unique identifier for each row
+    OrderID INT,
+    CustomerName VARCHAR(100),
+    Product VARCHAR(100)
+);
+
+-- Insert normalized data into Products table
+INSERT INTO Products (OrderID, CustomerName, Product)
+VALUES
+    (101, 'John Doe', 'Laptop'),
+    (101, 'John Doe', 'Mouse'),
+    (102, 'Jane Smith', 'Tablet'),
+    (102, 'Jane Smith', 'Keyboard'),
+    (102, 'Jane Smith', 'Mouse'),
+    (103, 'Emily Clark', 'Phone');
+
+
+-- Qn 2 Achieving 2NF (Second Normal Form)
+
+-- Create Orders table
+CREATE TABLE Orders (OrderID INT PRIMARY KEY,
+CustomerName VARCHAR(100)
+);
+
+-- Insert data into Orders table
+INSERT INTO ORDERS (OrderID, CustomerName)
+VALUES (101, 'John Does'), (102, 'Jane Smith'), (103, 'Emily Clark');
+
+
+-- Create OrderProducts Table
+CREATE TABLE OrderProducts (
+    OrderID INT, Product VARCHAR(100), Quantity INT, 
+    PRIMARY KEY (OrderID, Product),
+    FOREIGN KEY (OrderID) REFERENCES Orders(OrderID)
+);
+-- Insert data into OrderProducts table
+INSERT INTO OrderProducts (OrderID, Product, Quantity)
+VALUES
+    (101, 'Laptop', 2),
+    (101, 'Mouse', 1),
+    (102, 'Tablet', 3),
+    (102, 'Keyboard', 1),
+    (102, 'Mouse', 2),
+    (103, 'Phone', 1);
+
+
